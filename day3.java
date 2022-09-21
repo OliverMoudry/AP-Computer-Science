7a903c159ba076e44825ec6aababa9931a8e8ddb
OliverMoudry <94993514+OliverMoudry@users.noreply.github.com>
2022-09-20 22:55:13 -0700

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util;
+
+public class day3 {
+
+    public static String questionQ(){
+        Random rand = new Random();
+            int randQuestion = rand.nextInt(6);
+            String[] questionList = {"Please enter a ", "Enter a ", "I need a ", "Give me a ",
+                    "I would like a ", "The computer requires a "};
+        String question = Arrays.asList(questionList).indexOf(randQuestion);
+                return question;
+
+    }
+
+    
+
+
+
+
+    public static String questionWord() {
+        String[] wordList = { "verb", "noun", "plural noun", "past tense verb", "adjective", "historic event", "adverb",
+                "name of someone in the room", "measurement", "measurement in time" };
+        for (int i = 0; i < wordList.length; i++) {
+            String word = findIndex(wordList, i);
+            return word;
+        }
+        
+
+    }
+
+    public static String question(String questionWord, String questionQ) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println(questionQ() + " " + word + " please.");
+        String speechPart = keyboard.nextLine();
+        return speechPart;
+    }
+
+    public static void main(String[] args) {
+       questionQ();
+       questionWord();
+       question(questionWord(),questionQ());
+
+
+        
+
+        //System.out.println("Here is the story you created: ");
+       /*System.out.println("Once upon a " + noun1 + " there was a " + animal1 + " who was named "
+                + personName1 + ". One fine summer " + noun2 + " , " + personName1 + " was " +
+                verb1 + "and they saw a " + adjective1 + " " + noun3 +
+                "moving in their direction. " + personName1 + "got really " + adjective2 + ". ");
+
+        /*
+         * "Once upon a " + noun1 + " there was a " + animal1 + " who was named "
+         * +personName1 + ". One fine summer " + noun2 + " , " + personName1 + " was " +
+         * verb1 + "and they saw a " + adjective1 + " " + noun3 +
+         * "moving in their direction. " + personName1 + "got really " + adjective2 +
+         * ". "
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+    }
+}
---
+++ day4.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.annotation.processing.SupportedOptions;
+import javax.security.auth.Subject;
+
+import java.util;
+
+public class day4 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a word lol");
+        String word = keyboard.nextLine();
+        System.out.println(word + " has " + word.length() + " chars :(");
+        System.out.println(word + "'s 1st character is " + word.charAt(0));
+        System.out.println(word.toUpperCase() + ".");
+        System.out.println(word.toLowerCase() + ".");
+        boolean containsThe = word.contains("The");
+        System.out.println(word + " contains \' The \' is " + containsThe);
+        System.out.println(word + " contains the following letters: " + word.substring(0,word.length()));
+
+        int length = word.length();
+        int halfway = length/2;
+        int third = length/3;
+        int secondThird = length*2/3;
+        int lastThird = length*3/3;
+
+        System.out.println(word + " is " + length + " characters long");
+        System.out.println("The middle letter is " + halfway);
+        System.out.println("The first third of the word is " + third);
+        System.out.println("The middle third is " + secondThird);
+        System.out.println("The last third is " + lastThird);
+
+    }
+
+
+
+
+}
