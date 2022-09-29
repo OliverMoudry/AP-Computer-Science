efb2f8ae752e00709d3f01a8959cf0a246fcb53e
OliverMoudry <94993514+OliverMoudry@users.noreply.github.com>
2022-09-29 11:35:21 -0700

    Update adventure.java

--- adventure.java
+++ adventure.java
@@ -14,19 +14,21 @@ public class adventure {
   }
 
   // Random number
-  public static int random(int bound){
+  public static int random(int bound) {
     Random rand = new Random();
-     int randnumb = rand.nextInt(bound);
+    int randnumb = rand.nextInt(bound);
     return randnumb;
   }
-  
-  public static void main (String args[]) {
+
+  public static void main(String args[]) {
     Scanner keyboard = new Scanner(System.in);
-    /* choose your own adventure that needs at least 4 choices per path. one input needs to be an integer. need while loops to recieve valid input 
+    /*
+     * choose your own adventure that needs at least 4 choices per path. one input
+     * needs to be an integer. need while loops to recieve valid input
      * 
-     * indentation, semicolon, 
+     * indentation, semicolon,
      * 
-    */
+     */
 
     print("Please enter a username to continue. ");
     String username = keyboard.nextLine();
@@ -36,7 +38,8 @@ public class adventure {
 
     println("Hello " + username + ", and welcome back to the ");
 
-    println("You are on a " + random(500) + " km walk in the middle of nowhere and you are close to dying of exaustion. You see a forest to your left and a desert with an oasis to your right. 1: Go to the left into the forest where you might find food. 2: Go to the right into the desert toward the oasis where you could get water. Please type 1 for Forest and 2 for Desert.");
+    println("You are on a " + random(500)
+        + " km walk in the middle of nowhere and you are close to dying of exaustion. You see a forest to your left and a desert with an oasis to your right. 1: Go to the left into the forest where you might find food. 2: Go to the right into the desert toward the oasis where you could get water. Please type 1 for Forest and 2 for Desert.");
     int choice = keyboard.nextInt();
     while (choice != 1 && choice != 2) {
       println("Try again bozo");
@@ -44,17 +47,33 @@ public class adventure {
     }
 
     if (choice == 1) {
-      print("You start walking toward the forest lugging your heavy backpack behind you. You keep walking and you walk another " + random(10) + " km into the forest until you finally find a river. The white water rushing over the rocks assures you that it is probably safe to drink. You bend down and cup your hands preparing to scoop some water when you hear a siren in the distance. You raise your head and try to find where the sound came from. Do you 1, stay and try to find food while making your way back out of the forest, or 2, try to investigate the sound. ");
-      
+      print(
+          "You start walking toward the forest lugging your heavy backpack behind you. You keep walking and you walk another "
+              + random(10)
+              + " km into the forest until you finally find a river. The white water rushing over the rocks assures you that it is probably safe to drink. You bend down and cup your hands preparing to scoop some water when you hear a siren in the distance. You raise your head and try to find where the sound came from. Do you 1, stay and try to find food, 2, try to investigate the sound, or 3, try to find your way out of the forest?");
+      choice = keyboard.nextInt();
+      while (choice != 1 && choice != 2 && choice != 3) {
+        println("Try again bozo");
+        choice = keyboard.nextInt();
+      }
+      if (choice == 1) {
+        String input = "";
+        String input2 = "";
+            print("ERROR: 404. DUE TO TECHNICAL ISSUES YOU HAVE BEEN SIGNED OUT. PLEASE ENTER A USERNAME: ");
+        input = keyboard.nextLine();
+        println("PLEASE ENTER A PASSWORD: ");
+        input2 = keyboard.nextLine();
+        while (!input.equals(username) || !input2.equals(password)) {
+          println("YOUR USERNAME OR PASSWORD WAS INCORRECT. PLEASE TRY AGAIN");
+          println("PLEASE ENTER A USERNAME: ");
+          input = keyboard.nextLine();
+          println("PLEASE ENTER A PASSWORD: ");
+          input2 = keyboard.nextLine();
+        }
 
+      }
     }
 
-
-    
-    
-  
-  
   }
-
 
 }
