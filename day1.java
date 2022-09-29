cef0d2938d4dfa6ed4c33cbf05ee8cfdcd1c0029
OliverMoudry <94993514+OliverMoudry@users.noreply.github.com>
2022-09-29 11:27:36 -0700

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util;
+
+public class Main {
+    public static void main (String args[]){
+
+		System.out.println("Your First Name: ");
+		Scanner firstName = new Scanner(System.in);
+        Scanner lastName = new Scanner(System.in);
+
+		String input = firstName.nextLine();
+        String input1 = lastName.nextLine();
+		
+        System.out.println("Hello, " + lastName + firstName + ".");
+
+			
+			
+		
+		
+		
+		
+		
+	
+	}
+
+    
+}
