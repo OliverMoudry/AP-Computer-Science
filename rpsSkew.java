cd2a434aec56a15423156234b842224c82258e32
OliverMoudry <94993514+OliverMoudry@users.noreply.github.com>
2022-10-11 21:23:12 -0700

    Update rpsSkew.java

--- rpsSkew.java
+++ rpsSkew.java
@@ -1,1 +1,18 @@
+else if (difficulty == 3) {
+        println("Please choose Rock, Paper, or Scissors.");
+        String playerChoice = keyboard.nextLine();
+        while (!playerChoice.equals("Rock") && !playerChoice.equals("Paper") && !playerChoice.equals("Scissors")) {
+            println("I need either Rock, Paper, or Scissors.");
+            playerChoice = keyboard.nextLine();
+        }
+        if (playerChoice.equals("Rock")) {
+          String[] PS = {"Rock", "Scissors"};
+          int computerRandom = random(PS.length);
+          String[] computerChoices = {"Paper", PS[computerRandom]};
+          String finalComChoice = computerChoices[random(computerChoices.length)];
+          
+        }
 
+
+        
+    }
