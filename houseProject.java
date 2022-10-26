56c79d85350385502a56856402528efc6ecf9851
OliverMoudry <94993514+OliverMoudry@users.noreply.github.com>
2022-10-26 12:00:16 -0700

    Update houseProject.java

--- houseProject.java
+++ houseProject.java
@@ -6,6 +6,7 @@ import java.awt.Point;
 import javax.swing.SwingUtilities;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.Random;
 
 import com.sun.tools.javac.Main;
 
@@ -31,11 +32,16 @@ public class houseProject extends JPanel {
     return rand.nextInt(bound);
   }
 
+  public void setColor(Graphics graphics, int r, int g, int b) {
+    Color customColor = new Color(r, g, b);
+    graphics.setColor(customColor);
+  }
+
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     int SCREEN_WIDTH = getSize().width;
     int SCREEN_HEIGHT = getSize().height;
-    g.setColor(Color.BLUE.brighter().brighter().brighter().brighter());
+    //setColor(g, random(255), random(255), random(255));
     g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     g.setColor(Color.GREEN.darker());
@@ -44,22 +50,24 @@ public class houseProject extends JPanel {
     g.fillRect((SCREEN_WIDTH / 3), (SCREEN_HEIGHT / 3), 500, 400); // house
 
     for (int i = (SCREEN_WIDTH / 3) + 100; i < (SCREEN_WIDTH / 3) + 500; i += 200) { // windows
-      g.setColor(Color.WHITE);
+      setColor(g, random(255), random(255), random(255));
       g.fillRect(i, SCREEN_HEIGHT / 3 + 50, 100, 100);
     }
+    g.setColor(Color.BLACK);
+    g.fillOval(SCREEN_WIDTH/3 + 100, SCREEN_HEIGHT/3 + 50, SCREEN_WIDTH, SCREEN_HEIGHT);
     g.setColor(Color.BLACK);
     g.drawLine(SCREEN_WIDTH / 3 + 150, SCREEN_HEIGHT / 3 + 400, SCREEN_WIDTH / 3 + 150, SCREEN_HEIGHT / 3 + 250); // door
     g.drawLine(SCREEN_WIDTH / 3 + 250, SCREEN_HEIGHT / 3 + 400, SCREEN_WIDTH / 3 + 250, SCREEN_HEIGHT / 3 + 250);
     g.fillArc(SCREEN_WIDTH / 3 + 150, SCREEN_HEIGHT / 3 + 200, 100, 100, 0, 180);
     g.setColor(Color.ORANGE.darker().darker().darker());
-    drawTriangle(g, SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3, SCREEN_WIDTH / 3 + 500, SCREEN_HEIGHT / 3, SCREEN_WIDTH / 2,
+    drawTriangle(g, SCREEN_WIDTH / 3 - 50, SCREEN_HEIGHT / 3, SCREEN_WIDTH / 3 + 550, SCREEN_HEIGHT / 3, SCREEN_WIDTH / 2,
         SCREEN_HEIGHT / 4); // roof
         g.setColor(Color.GREEN);
     g.fillRect(SCREEN_WIDTH/4, SCREEN_HEIGHT/4 *3, 300, 100); // bush
     g.fillRect(SCREEN_WIDTH/4 * 2, SCREEN_HEIGHT/4 * 3, 300, 100); // bush
     g.setColor(Color.ORANGE.darker().darker().darker());
-    g.fillRect(SCREEN_WIDTH/4, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT); // tree trunk NOT FINISHED!
-    g.fillOval(SCREEN_HEIGHT, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT);// tree leaves NOT FINISHED!
+    //g.fillRect(SCREEN_WIDTH/4, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT); // tree trunk NOT FINISHED!
+    //g.fillOval(SCREEN_HEIGHT, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT);// tree leaves NOT FINISHED!
   }
 
   public void drawTriangle(Graphics g, int x1, int y1, int x3, int y3, int x2, int y2) {
@@ -78,5 +86,9 @@ public class houseProject extends JPanel {
     frame.getContentPane().add(panel, BorderLayout.CENTER);
     frame.setVisible(true);
 
+
+    while (true) {
+      panel.repaint();
+    }
   }
 }
